69a59047400d1364733f7521635c2a3b367f1e0c
filippo <riccardo.gabba02@gmail.com>
2023-10-18 17:01:57 +0200

    finished the second assignment

--- array_tail/script.js
+++ array_tail/script.js
@@ -1,8 +1,18 @@
-var foo = [];
-
-
-for (var i = 1; i <= 100; i++) {
-   foo.push(i);
-}
-
-console.log(foo)
+function casuali() {
+   let riomare = [];
+   
+    n = parseInt(document.getElementById("quanti").value);
+    if (n > 0 && n <= 100) {
+      str = "";
+      for (i = 0; i < n; i++) {
+           num = Math.round(Math.random()*100 + 1);
+           
+           riomare.push(num);
+       }
+      console.log(riomare) 
+      document.getElementById("casuali").innerHTML = riomare.slice(riomare.length - 5, riomare.length);
+     } 
+     else {
+        document.getElementById("casuali").innerHTML = "Inserisci un numero maggiore di 0 e inferiore a 100";
+     }
+  }
